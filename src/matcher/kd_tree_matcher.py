70535711ccf00c66ff753043e1fc39b357829ba8
Weronika Wolska <weronikawolska7@gmail.com>
2021-03-10 01:02:22 +0100

    Create kd_tree to find similar images in a given dataset

---
+++ src/matcher/kd_tree_matcher.py
@@ -0,0 +1,26 @@
+from sklearn.neighbors import KDTree
+
+
+class KDTreeMatcher:
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        # dataset is a tuple with vectors on position [1] which we need to build the tree
+        # vectors are of size 4096
+        self.tree = KDTree(list(map(lambda val: val[1], dataset)), leaf_size=4096)
+
+    def find_neighbours(self, vectors):
+        """
+        :param vectors: vectors to be matched
+        :return: list with matched images' names
+        """
+        dist, index = self.tree.query([vectors], k=10)
+        index = index[0]
+        return list(map(self.__map_index, index))
+
+    def __map_index(self, index):
+        """
+        :param index: tuple's index
+        :return: name of the file
+        """
+        return self.dataset[index][0]
