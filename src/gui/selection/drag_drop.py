2122723fb9bc1eb99091e54125af33094e15cfae
Weronika Wolska <weronikawolska7@gmail.com>
2021-03-15 14:51:11 +0100

    confirm button working

--- src/gui/selection/drag_drop.py
+++ src/gui/selection/drag_drop.py
@@ -1,24 +1,25 @@
 from PyQt5.QtGui import QPixmap
 from gui.selection.image_label import ImageLabel
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QFrame
 
 
 class DragAndDrop(QWidget):
     def __init__(self, connector):
-        size = 900
         super().__init__()
-        # self.file_path = None
+        self.file_path = None
         self.connector = connector
-        self.resize(size, size)
-        self.setAcceptDrops(True)
-        # button = self.QPushButton(self)
-        # button.setText("Confirm")
-
-        main_layout = QVBoxLayout()
-
         self.photo_viewer = ImageLabel()
+        self.create_widget()
+
+    def create_widget(self):
+        main_layout = QVBoxLayout()
+        self.setGeometry(200, 200, 300, 300)
+        self.setAcceptDrops(True)
+        button = self.create_button()
+
         main_layout.addWidget(self.photo_viewer)
+        main_layout.addWidget(button)
         self.setLayout(main_layout)
 
     def dragEnterEvent(self, event):
@@ -37,11 +38,21 @@ class DragAndDrop(QWidget):
         if event.mimeData().hasImage:
             event.setDropAction(Qt.CopyAction)
             file_path = event.mimeData().urls()[0].toLocalFile()
+            self.file_path = file_path
             self.set_image(file_path)
-            print(self.connector.find_images(file_path))
             event.accept()
         else:
             event.ignore()
 
     def set_image(self, file_path):
         self.photo_viewer.setPixmap(QPixmap(file_path))
+
+    def create_button(self):
+        button = QPushButton(self)
+        button.setText("Confirm")
+        button.clicked.connect(self.clicked)
+        return button
+
+    def clicked(self):
+        # TODO print images in gui list
+        print(self.connector.find_images(self.file_path))
