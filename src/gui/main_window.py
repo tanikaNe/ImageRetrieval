5956ccdfcff2b1e57e837a55ab5a7bc7fafef7ad
Weronika Wolska <weronikawolska7@gmail.com>
2021-04-11 23:11:25 +0200

    display results in a grid

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QDesktopWidget
 
 from src.gui.results.results_list import ResultsList
 from src.gui.selection.drag_drop import DragAndDrop
@@ -9,6 +9,8 @@ class MainWindow(QMainWindow):
     def __init__(self, connector):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Similar Image Finder")
+        self.center()
+        self.resize(1000, 1000)
 
         self.connector = connector
 
@@ -22,8 +24,14 @@ class MainWindow(QMainWindow):
         self.result_widget = None
 
     def setResults(self, results):
-        if self.result_widget is not None:
+        if self.result_widget:
             self.layout.removeWidget(self.result_widget)
 
         self.result_widget = results
         self.layout.addWidget(self.result_widget)
+
+    def center(self):
+        frame = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(center_point)
+        self.move(frame.topLeft())
--- src/gui/results/results_list.py
+++ src/gui/results/results_list.py
@@ -1,25 +1,44 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QScrollArea, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QScrollArea, QLabel, QScrollBar, QListWidget, QGroupBox, QGridLayout, \
+    QListWidgetItem, QHBoxLayout
 
 
 class ResultsList(QWidget):
     def __init__(self, images):
         super().__init__()
         self.vbox = QVBoxLayout()
-        # self.scroll = QScrollArea()
         self.initUI(images)
 
     def initUI(self, images):
         size = 100
-        for img in images:
-            image_label = QLabel(self)
-            image_label.setPixmap(QPixmap(img).scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation))
-            self.vbox.addWidget(image_label)
-            self.setLayout(self.vbox)
+        row_index = 0
+        column_index = 0
+        results_list_layout = QGridLayout()
+        if images:
+            for img in images:
+                image_label = QLabel(self)
+                image_label.setPixmap(QPixmap(img).scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+                image_widget = QWidget()
+                image_layout = QHBoxLayout()
+                image_layout.setAlignment(Qt.AlignCenter)
+                image_layout.addWidget(image_label)
+                image_widget.setLayout(image_layout)
+                results_list_layout.addWidget(image_widget, row_index, column_index)
 
-            # Scroll Area
-        # self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        # self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        # self.scroll.setWidgetResizable(True)
-        # self.scroll.setWidget(self)
+                column_index += 1
+                if column_index == 2:
+                    column_index = 0
+                    row_index += 1
+
+            print(results_list_layout)
+
+        list_widget = QWidget()
+        list_widget.setLayout(results_list_layout)
+        scroll = QScrollArea()
+        scroll.setWidget(list_widget)
+        scroll.setWidgetResizable(True)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(scroll)
+        self.setLayout(layout)
